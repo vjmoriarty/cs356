f2177ff4740bc38f6c624c09fa0c93be6878458e
Vincent Yu <yyu@haverford.edu>
2021-04-14 15:41:47 -0400

    Update jungle.java

--- src/jungle/jungle.java
+++ src/jungle/jungle.java
@@ -187,7 +187,7 @@ class Ladder {
                 oppoDirection.signalAll();
             } else {
                 // If this is just a regular scenario, notify the next ape waiting to grab the released rung
-                nextRung.signal();
+                nextRung.signalAll();
             }
 
         } finally {
@@ -297,12 +297,12 @@ class Jungle {
         //    timing configuration should work, and there should be no way to
         //    add spurious "tryToSleep"'s *anywhere* to mess it up.
         //
-        int    eastBound = 4; // how many apes going East? use -1 for inifinity
-        int    westBound = 2; // how many apes going West? use -1 for inifinity
-        double apeMin = 4.0;  // how long to wait between consecutive apes going one way
-        double apeVar = 1.0;  // 4 seconds is usually enough, but vary a bit to see what happens
-        double sideMin = 5.0; // how long to wait before coming back across
-        double sideVar = 0.0; // 5.0 seconds is usually enough
+        int eastBound = 10;     // how many apes going East? use -1 for inifinity
+        int westBound = 10;     // how many apes going West? use -1 for inifinity
+        double apeMin = 4.0;    // how long to wait between consecutive apes going one way
+        double apeVar = 1.0;    // 4 seconds is usually enough, but vary a bit to see what happens
+        double sideMin = 5.0;   // how long to wait before coming back across
+        double sideVar = 0.0;   // 5.0 seconds is usually enough
 
         // create a Ladder
         Ladder l = new Ladder(4, true);
